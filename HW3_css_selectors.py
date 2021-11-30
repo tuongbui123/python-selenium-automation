66f2ccd81dbaec88f4a61770bba8e2e254253aa7
Tuong <tuongbui2110@gmail.com>
2021-11-29 23:36:56 -0800

    HW3_css_selectors.py

---
+++ HW3_css_selectors.py
@@ -0,0 +1,30 @@
+from selenium import webdriver
+from selenium.webdriver.common.by
+import By
+
+driver = webdriver.Chrome()
+
+driver.find_element(By.CSS_SELECTOR, "i.a-icon a-icon-logo")
+
+driver.find_element(By.CSS_SELECTOR, "h1.a-spacing-small")
+
+driver.find_element(By.CSS_SELECTOR, "input#ap_customer_name")
+
+driver.find_element(By.CSS_SELECTOR, "input#ap_email")
+
+driver.find_element(By.CSS_SELECTOR, "input.a-input-text.a-form-normal.a-span12.auth-require-password-validation")
+
+driver.find_element(By.CSS_SELECTOR, "div.a-alert-inline-info.auth-inlined-information-message.a-spacing-top-mini a-alert-content")
+
+driver.find_element(By.CSS_SELECTOR, "input#ap_password_check")
+
+driver.find_element(By.CSS_SELECTOR, "input.a-button-input[type=submit]")
+
+driver.find_element(By.CSS_SELECTOR, "a[href='/gp/help/customer/display.html/ref=ap_register_notification_condition_of_use?ie=UTF8&nodeId=508088']")
+
+driver.find_element(By.CSS_SELECTOR, "a[href='/gp/help/customer/display.html/ref=ap_register_notification_privacy_notice?ie=UTF8&nodeId=468496']")
+
+driver.find_element(By.CSS_SELECTOR, "a.a-link-emphasis-a[href='/ap/signin?openid.pape.max_auth_age=0&openid.return_to=https%3A%2F%2Fwww.amazon.com%2Fgp%2Fyourstore%2Fhome%3Fie%3DUTF8%26action%3Dsign-out%26path%3D%252Fgp%252Fyourstore%252Fhome%26ref_%3Dnav_AccountFlyout_signout%26signIn%3D1%26useRedirectOnSuccess%3D1&prevRID=8EPTVFGP8R05B9GVJ5EQ&openid.identity=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.assoc_handle=usflex&openid.mode=checkid_setup&openid.ns.pape=http%3A%2F%2Fspecs.openid.net%2Fextensions%2Fpape%2F1.0&failedSignInCount=0&openid.claimed_id=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&pageId=usflex&openid.ns=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0']")
+
+
+
